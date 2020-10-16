ae15cb0be912a2efab4744095479431033ec6497
Fatma <fceyda@gmail.com>
2020-10-16 14:21:30 -0700

    create component placeholders

---
+++ screens/ReviewDetails.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+function ReviewDetails() {
+  return (
+    <View style={styles.container}>
+      <Text>ReviewDetails Screen</Text>
+    </View>
+  )
+}
+
+const styles= StyleSheet.create({
+  container: {
+    padding: 24,
+
+  },
+})
+
+export default ReviewDetails
