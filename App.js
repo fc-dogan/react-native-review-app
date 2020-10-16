a122176171901ddd43d12717dc6c5b1dcf0e5c2a
Fatma <fceyda@gmail.com>
2020-10-16 14:58:19 -0700

    create globalStyles file and import google font

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Fonts from 'expo-font';
+import { AppLoading } from 'expo';
 import Home from './screens/Home'
 
+const getFonts = () => Fonts.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+});
+
 export default function App() {
-  return (
-    <Home />
-  );
+  const[fontsLoaded, setFontsLoaded] = useState(false)
+
+  if(fontsLoaded){
+    return (
+      <Home />
+    );
+
+  } else {
+    return (
+      <AppLoading 
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    )
+  }
 }
 
 const styles = StyleSheet.create({
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import { StyleSheet, View, Text } from 'react-native'
+import { globalStyles } from '../styles/global'
 
 function ReviewDetails() {
   return (
-    <View style={styles.container}>
+    <View style={globalStyles.container}>
       <Text>ReviewDetails Screen</Text>
     </View>
   )
 }
 
-const styles= StyleSheet.create({
-  container: {
-    padding: 24,
-
-  },
-})
 
 export default ReviewDetails
