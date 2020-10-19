5d0b177aac55c4b5d1f593fae82bfbac1303a1cb
Fatma <fceyda@gmail.com>
2020-10-19 12:40:16 -0700

    pass data to review details screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,28 @@
-import React from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native'
+import React, {useState} from 'react'
+import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native'
 import { globalStyles } from '../styles/global'
 
 function Home({ navigation }) {
-
-  const pressHandler =() => {
-    navigation.navigate('Details');
-    // navigation.push('Details');
-  }
+  
+  const [reviews, setReviews] = useState([
+    { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+    { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
+    { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
+  ]);
+  // const pressHandler =() => {
+  //   navigation.navigate('Details');
+  //   // navigation.push('Details');
+  // }
 
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home Screen</Text>
-      <Button title='go to reviews' onPress={pressHandler}/>
+      <FlatList
+       data={reviews}
+       renderItem={({ item }) => (
+         <TouchableOpacity onPress={ () => navigation.navigate('Details', item )}>
+           <Text styles={globalStyles.titleText}>{ item.title }</Text>
+         </TouchableOpacity>
+       )} />
     </View>
   )
 }
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -2,16 +2,18 @@ import React from 'react'
 import { StyleSheet, View, Text, Button } from 'react-native'
 import { globalStyles } from '../styles/global'
 
-function ReviewDetails({ navigation }) {
-
-  const pressHandler = () => {
-    navigation.goBack();
-  }
+function ReviewDetails({ route, navigation }) {
+  const { title, body, rating } = route.params;
+  // const pressHandler = () => {
+  //   navigation.goBack();
+  // }
 
   return (
     <View style={globalStyles.container}>
-      <Text>ReviewDetails Screen</Text>
-      <Button title='back to home screen' onPress={pressHandler}/>
+      <Text>{title}</Text>
+      <Text>{body}</Text>
+      <Text>{rating}</Text>
+      {/* <Button title='back to home screen' onPress={pressHandler}/> */}
     </View>
   )
 }
